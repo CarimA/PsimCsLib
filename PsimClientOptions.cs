839491dd0b48c28a77302d658450c249e5f6bbf2
Carim A <carim@glaciate.net>
2024-05-15 23:52:09 +0100

    Update websocket endpoint

--- PsimClientOptions.cs
+++ PsimClientOptions.cs
@@ -4,10 +4,10 @@ public sealed class PsimClientOptions
 {
 	public string Username { get; set; }
 	public string Password { get; set; }
-	public string ServerAddress { get; set; } = "sim.smogon.com";
+	public string ServerAddress { get; set; } = "sim3.psim.us";
 	public string LoginServer { get; set; } = "https://play.pokemonshowdown.com/~~showdown/action.php";
-	public bool SecureWebsocketConnection { get; set; } = false;
-	public int Port { get; set; } = 8000;
+	public bool SecureWebsocketConnection { get; set; } = true;
+	public int Port { get; set; } = 443;
 
 	internal string ToServerUri()
 	{
